55aecd212c81d3b66ca64b8cd9c5044c290f11b4
jaye <jaye1248276@gmail.com>
2023-03-04 23:09:31 +0800

    [update]: 添加dts插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
+  plugins: [
+    dts()
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'miniprogram/src/simpleAxios/index.ts'),
